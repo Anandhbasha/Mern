0d60ddba88d5a86c98885ef37e758e8a17e7f1cb
Anandhbasha <anadhakm@gmail.com>
2024-11-21 08:35:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+//Hoisting
+//var,let,const
+// var x=5;
+// let y=10;
+// const z=15;
+//  y = x+1;
+// console.log(x/y,z);
+
+
+// //function
+// function click(){
+//     console.log(x*y*z);
+  
+// }
+
+// click();
+
+
+//If statement
+// let name = "anandh";
+// let age =1;
+// //const gender= "Male";
+
+// if(gender == "Male"){
+//     console.log("He is Male");
+    
+// }
+// else{
+//     console.log("He is not a Male");
+// }
+
+// var name1 = "amir";
+// var mark1 = 420;
+// var name2 = "partha";
+// var mark2 = 452;
+// var name3 = "akash";
+// var mark3 = 380;
+// var name4 = "karthick";
+// var mark4 = 460;
+// var name5 = "kavin";
+// var mark5 = 440;
+
+//array[] 
+//object{}
+
+// const person = [{
+//     name:"amir",
+//     mark:420
+// },
+// {
+//     name:"partha",
+//     mark:452
+// },
+// {
+//     name:"akash",
+//     mark:380
+// },
+// {
+//     name:"karthick",
+//     mark:460
+// },
+// {
+//     name:"kavin",
+//     mark:440
+// },]
+// // a = [1,2,3,4,5,6,7,8]
+// // console.log(a[7]);
+// for(let i=0;i<=person.length;i++){
+//     let names = person[i];
+//     if(names.mark>450){
+//     console.log(names.name + " He is Topper");
+// }
+// else{
+//     console.log(names.name + " He is Average Student");
+// }
+    
+// }
+///operator,fuction,loop,statement,Hoisting,
+
+
+let x = 10;
+let y = 13;
+let z = 20;
+
+if(x%2==0 || y%2==0 && z%2==0){
+    console.log("all the numbers even numbers"); 
+}
+else{
+    console.log("Not a even number");
+    
+}
